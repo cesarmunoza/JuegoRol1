67adfbc3bb63753a2b14cc3a6f35f2cd82d1f5a8
César <Csar@FamiliaMunoz>
2016-11-15 22:59:05 -0500

    new commit hello world git java

---
+++ src/juegos/Juego.java
@@ -0,0 +1,121 @@
+package juegos;
+
+import control.Teclado;
+import java.awt.BorderLayout;
+import java.awt.Canvas;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+public class Juego extends Canvas implements Runnable{
+    
+    private static final int ANCHO = 800;
+    private static final int ALTO = 600;
+    
+    private static volatile boolean enFuncionamiento = false;
+    
+    private static final String NOMBRE = "Juego";
+    
+    private static int aps = 0;
+    private static int fps = 0;
+    
+    private static JFrame ventana;
+    private static Thread hilo;
+    private static Teclado teclado;
+    
+    private Juego(){
+        setPreferredSize(new Dimension(ANCHO, ALTO));
+        teclado = new Teclado();
+        addKeyListener(teclado);
+        ventana = new JFrame(NOMBRE);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setResizable(false);
+        ventana.setLayout(new BorderLayout());
+        ventana.add(this, BorderLayout.CENTER);
+        ventana.pack();
+        ventana.setLocationRelativeTo(null);
+        ventana.setVisible(true);
+    }
+    
+    public static void main(String[] args) {
+        Juego juego = new Juego();
+        juego.iniciar();
+    }
+    
+    private synchronized void iniciar(){
+        enFuncionamiento = true;
+        hilo = new Thread(this, "Gráficos");
+        hilo.start();
+    }
+    
+    private synchronized void detener(){
+        enFuncionamiento = false;
+        try {            
+            hilo.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private void actualizar(){
+        aps++;
+    }
+    
+    private void mostrar(){
+        teclado.actualizar();
+        
+        if(teclado.arriba){
+            System.out.println("Arriba");
+        }
+        
+        if(teclado.abajo){
+            System.out.println("Abajo");
+        }
+        
+        if(teclado.izquierda){
+            System.out.println("Izquierda");
+        }
+        
+        if(teclado.derecha){
+            System.out.println("Derecha");
+        }
+        
+        fps++;
+    }
+    
+    public void run() {
+        final int NS_POR_SEGUNDO = 1000000000;
+        final byte APS_OBJETIVO = 60;//Actualizaciones por segundo
+        final double NS_POR_ACTUALIZACION = NS_POR_SEGUNDO/APS_OBJETIVO;
+        
+        long referenciaActualizacion = System.nanoTime();
+        long referenciaContador = System.nanoTime();
+        
+        double tiempoTranscurrido;
+        double delta = 0;
+        
+        requestFocus();
+        
+        while(enFuncionamiento){
+            final long inicioBucle = System.nanoTime();
+            
+            tiempoTranscurrido = inicioBucle - referenciaActualizacion;
+            referenciaActualizacion = inicioBucle;
+            
+            delta += tiempoTranscurrido / NS_POR_ACTUALIZACION;
+            
+            while(delta >= 1){
+                actualizar();
+                delta--;
+            }                        
+            mostrar();
+            
+            if(System.nanoTime() - referenciaContador > NS_POR_SEGUNDO){
+                ventana.setTitle(NOMBRE + " || APS: " + aps + " || FPS: " + fps);
+                aps=0;
+                fps=0;
+                referenciaContador = System.nanoTime();
+            }
+        }
+    }
+    
+}
